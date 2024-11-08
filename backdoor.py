aed56e0020c069a62c50fabc0dc27d0ea4c564a0
davoodya <davoodya40@gmail.com>
2024-11-08 23:26:04 +0330

    Implement Step 2: Define Global Variables

--- backdoor.py
+++ backdoor.py
@@ -12,8 +12,46 @@ End Developing Date:
 
 import socket
 import os
+from queue import Queue
 from time import sleep
 import threading
 from sys import exit
+
+
+""" Section 1: Develop Utitlity Objects(Global Variables, lambda's, functions) """
+
+# Step 2: Define Global Variables
+
+intThreads = 2      # Thread's Number in Multithreading
+arrJobs = [1, 2]    # Use for Multithreading, Store Total Jobs
+
+queue = Queue()     # Used for Handling Jobs in Multithreading
+
+arrAddresses = []       # Store All Socket Connection Addresses
+arrConnections = []     # Store All Connection Information about Socket Connections
+
+strHost = "192.168.10.100"      # Server IP Address
+intPort = 4444                  # Server Port
+
+intBuff = 1024      # Maximum Size(Bytes) of Data to Receive as
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
