2c135b644e0de551900ab342f603e6da8e57eed6
davoodya <davoodya40@gmail.com>
2024-11-09 21:38:17 +0330

    Start Developing backdoor_client.py

---
+++ backdoor_client.py
@@ -0,0 +1,9 @@
+""" Yakuza Backdoor, Undetectable, Client Side
+        This is a Backdoor written in Python based on Yakuza Style
+        Version: 1.0
+------------------------------------------------------------
+Author: Davood Yakuza
+Start Developing Date: 11/9/2024 | 19 aban 1403.
+End Developing Date:
+
+"""
--- backdoor_server.py
+++ backdoor_server.py
@@ -1,9 +1,9 @@
-""" Yakuza Backdoor, Undetectable
+""" Yakuza Backdoor, Undetectable, Server Side
         This is a Backdoor written in Python based on Yakuza Style
         Version: 1.0
 ------------------------------------------------------------
 Author: Davood Yakuza
-Start Developing Date: 11/8/2024 | 18 aban 1403
+Start Developing Date: 11/8/2024 | 18 aban 1403.
 End Developing Date:
 
 """
