eb9575c9037527a0301322ea168db28807cbb06e
davoodya <davoodya40@gmail.com>
2024-11-09 21:42:08 +0330

    Implement Step 1: Import requires libraries

--- backdoor_client.py
+++ backdoor_client.py
@@ -7,3 +7,22 @@ Start Developing Date: 11/9/2024 | 19 aban 1403.
 End Developing Date:
 
 """
+
+# Step 1: Import requires libraries
+import socket
+import os
+from sys import argv, exit
+import platform
+from time import sleep
+import ctypes
+import subprocess
+import threading
+import wmi
+
+import win32api
+import winerror
+import win32event
+import win32crypt
+
+from winreg import *
+
