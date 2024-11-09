f6f41dc48d7d8741268079c2397c1447f98edc3e
davoodya <davoodya40@gmail.com>
2024-11-09 23:47:16 +0330

    Implement logic of Argument Handling 2

--- tmp.py
+++ tmp.py
@@ -1,12 +1,16 @@
 import os, sys
 
+filename = os.path.basename(sys.argv[0])
+filepath =os.path.realpath(sys.argv[0])
+print(f"File Name is: {filename}\nFile Path is: {filepath}")
 
-print(os.path.realpath(sys.argv[0]))
-
-print(sys.argv[1])
 
 try:
-    if not sys.argv[2]:
-        print("Enter Second argument")
-except IndexError:
-    print("Second argument Not Enter")
+    print(f"[+] First Argument is: {sys.argv[1]}")
+
+except IndexError: print("First Argument Not Enter.")
+
+try:
+    print(f"[+] Second Argument is: {sys.argv[2]}")
+
+except IndexError: print("Second Argument Not Enter.")
